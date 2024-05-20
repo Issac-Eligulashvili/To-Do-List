1d1380ece94dce2d3a306b26959c96b3118058d8
Issac-Eligulashvili <issac.eli2007@gmail.com>
2024-05-19 23:06:01 -0400

    to-do is done

--- js/app.js
+++ js/app.js
@@ -3,12 +3,24 @@ const clear = document.querySelector(".clear")
 const dateElement = document.getElementById('date')
 const list = document.getElementById('list')
 const input = document.getElementById('input')
+const CHECK = 'fa-check-circle'
+const UNCHECK = 'fa-circle-thin'
+const LINE_THROUGH = 'lineThrough'
 
-function addToDo(toDo) {
+let LIST = [];
+let id = 0;
+
+function addToDo(toDo, id,done, trash ) {
+
+if(trash) { return; }
+
+const DONE = done ? CHECK : UNCHECK;
+const LINE = done ? LINE_THROUGH : "";
+
     const text = `<li class="item">
-    <i class="co fa fa-circle-thin" job = "complete" ></i>
-    <p class="text">${toDo}</p>
-    <i class="de fa fa-trash-o" job = "delete"></i>
+    <i class="fa ${DONE}" job = "complete" id="${id}"></i>
+    <p class="text ${LINE}">${toDo}</p>
+    <i class="de fa fa-trash-o" job = "delete" id="${id}></i>
 </li>`
 
 const position = "beforeend";
@@ -40,7 +52,9 @@ document.addEventListener("keyup", function(event){
     }
 })
 
-//Store a To-Do
-
-let LIST = [];
-let id = 0;
+function completeToDo(element) {
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK)
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH )
+    LIST[element.id].done = LIST[element.id].done ? false : true
+}
