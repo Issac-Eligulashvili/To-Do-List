8a96a4818e52dac2683f9b4af1b0620ed8168e22
Issac-Eligulashvili <issac.eli2007@gmail.com>
2024-05-19 22:49:39 -0400

    insertAdjacentHTML

--- js/app.js
+++ js/app.js
@@ -3,3 +3,18 @@ const clear = document.querySelector(".clear")
 const dateElement = document.getElementById('date')
 const list = document.getElementById('list')
 const input = document.getElementById('input')
+
+function addToDo(toDo) {
+    const text = `<li class="item">
+    <i class="co fa fa-circle-thin" job = "complete" ></i>
+    <p class="text">${toDo}</p>
+    <i class="de fa fa-trash-o" job = "delete"></i>
+</li>`
+
+const position = "beforeend";
+
+list.insertAdjacentHTML(positiion, text)
+
+}
+
+addToDo("Drink Coffee");
